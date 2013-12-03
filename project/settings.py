bd49e104ecebf069f2d1d04e99539f3c3d6fc6a5
gillian-ubuntu <gillian@ubuntu.(none)>
2013-12-03 18:55:43 -0500

    deploy

--- project/settings.py
+++ project/settings.py
@@ -15,7 +15,10 @@ ADMINS = (
 MANAGERS = ADMINS
 
 import dj_database_url
-DATABASES['default'] = dj_database.url.config()
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
